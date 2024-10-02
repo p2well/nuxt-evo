f16f0bde1823384e41fed64bc8f1a4bff5c91463
Pawel Pawluk <p2well@outlook.com>
2024-10-02 15:17:10 +0200

    Update title

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
+          <h1>🧬 Nuxt Evolution</h1>
           <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
         </div>
       </template>
