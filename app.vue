17f34733084517ea3085079e675939639e0e907f
Pawel Pawluk <p2well@outlook.com>
2024-10-02 15:13:19 +0200

    Add initial horizontal navigation

--- app.vue
+++ app.vue
@@ -1,3 +1,38 @@
 <template>
+  <UHorizontalNavigation
+    :links="links"
+    class="border-b border-gray-200 dark:border-gray-800"
+  />
   <NuxtPage />
 </template>
+
+<script setup lang="ts">
+const links = [
+  [
+    {
+      label: 'Home',
+      icon: 'i-heroicons-home',
+      to: '/',
+    },
+    {
+      label: 'Horizontal Navigation',
+      icon: 'i-heroicons-chart-bar',
+    },
+    {
+      label: 'Command Palette',
+      icon: 'i-heroicons-command-line',
+    },
+  ],
+  [
+    {
+      label: 'Examples',
+      icon: 'i-heroicons-light-bulb',
+    },
+    {
+      label: 'Help',
+      icon: 'i-heroicons-question-mark-circle',
+      to: '/about'
+    },
+  ],
+];
+</script>
