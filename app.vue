a75c4e694328e1aa140e07818af0daf165b215e5
Pawel Pawluk <p2well@outlook.com>
2025-01-21 09:29:14 +0100

    Refactor header to a component

--- app.vue
+++ app.vue
@@ -1,54 +1,6 @@
 <template>
   <UContainer>
-    <a href="#main-content" class="skip-to-content">Skip to content</a>
-    <header class="flex flex-row items-center">
-      <div class="font-sans font-medium text-lg text-nowrap pr-4">
-        Nuxt Evolution
-      </div>
-      <UHorizontalNavigation
-        :links="links"
-        class="border-b border-gray-200 dark:border-gray-800"
-      />
-      <ColorModePicker />
-    </header>
+    <AppHeader />
     <NuxtPage />
   </UContainer>
 </template>
-
-<script setup lang="ts">
-const links = [
-  [
-    {
-      label: 'Home',
-      icon: 'i-heroicons-home',
-      to: '/',
-    },
-    {
-      label: 'Users',
-      icon: 'i-heroicons-user-group',
-      to: '/users',
-    },
-    {
-      label: 'About',
-      icon: 'i-heroicons-information-circle',
-      to: '/about',
-    },
-  ],
-];
-</script>
-
-<style scoped>
-.skip-to-content {
-  position: absolute;
-  top: -40px;
-  left: 0;
-  background: #000;
-  color: #fff;
-  padding: 8px;
-  z-index: 100;
-}
-
-.skip-to-content:focus {
-  top: 0;
-}
-</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="flex flex-row items-center">
+    <a href="#main-content" class="skip-to-content">Skip to content</a>
+    <div class="font-sans font-medium text-lg text-nowrap pr-4">
+      Nuxt Evolution
+    </div>
+    <UHorizontalNavigation
+      class="border-b border-gray-200 dark:border-gray-800"
+      :links="links"
+    />
+    <ColorModePicker />
+  </header>
+</template>
+
+<script setup lang="ts">
+const links = [
+  [
+    {
+      label: 'Home',
+      icon: 'i-material-symbols-home-outline',
+      to: '/',
+    },
+    {
+      label: 'Users',
+      icon: 'i-material-symbols-group-outline',
+      to: '/users',
+    },
+    {
+      label: 'About',
+      icon: 'i-material-symbols-info-outline',
+      to: '/about',
+    },
+  ],
+];
+</script>
+
+<style scoped>
+.skip-to-content {
+  position: absolute;
+  top: -40px;
+  left: 0;
+  background: #000;
+  color: #fff;
+  padding: 8px;
+  z-index: 100;
+}
+
+.skip-to-content:focus {
+  top: 0;
+}
+</style>
