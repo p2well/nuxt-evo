0fd299104f2a2d19d56489d378ef293aa7e41456
Pawel Pawluk <p2well@outlook.com>
2025-01-21 11:20:16 +0100

    Ensure that navigation wraps on small screen

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex flex-row items-center">
+  <header class="flex flex-row flex-wrap sm:flex-nowrap items-center">
     <a href="#main-content" class="skip-to-content">Skip to content</a>
     <div class="font-sans font-medium text-lg text-nowrap pr-4">
       Nuxt Evolution
